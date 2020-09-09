8c196cf76d9fb67f8844dec742446403c6e64845
Leon Ask <leon.ask@outlook.com>
2020-09-09 19:35:09 +0200

    safer invoke

--- src/Server/Services/DocEvents.cs
+++ src/Server/Services/DocEvents.cs
@@ -7,8 +7,8 @@ namespace Server
         public event Action<DocDto> OnNewDoc;
         public event Action<DocDto> OnUpdatedDoc;
         public event Action<DocDto> OnDeletedDoc;
-        public void NewDoc(DocDto dto) => OnNewDoc(dto);
-        public void UpdateDoc(DocDto dto) => OnUpdatedDoc(dto);
-        public void DeletedDoc(DocDto dto) => OnDeletedDoc(dto);
+        public void NewDoc(DocDto dto) => OnNewDoc?.Invoke(dto);
+        public void UpdateDoc(DocDto dto) => OnUpdatedDoc?.Invoke(dto);
+        public void DeletedDoc(DocDto dto) => OnDeletedDoc?.Invoke(dto);
     }
 }
